fdaaaa6e7d0dbbf26a3d889be39875e52bb7e2cf
anx450z <anx450z@gmail.com>
2022-08-24 11:22:37 +0530

    bug- Error: DataSource is not set for this entity -10

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,24 +3,27 @@ import { DataSource } from 'typeorm'
 import { User } from './entity/User'
 import dotenv from 'dotenv'
 import { Flash } from './entity/Flash'
+import { join } from 'path'
 // import * as PostgressConnectionStringParser from 'pg-connection-string'
 
 dotenv.config()
 const databaseUrl: string = process.env.DATABASE_URL
 // const connectionOptions =PostgressConnectionStringParser.parse(databaseUrl)
 export const AppDataSource = new DataSource({
-  // url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
   type: 'postgres',
-  host: process.env.HOST,
-  port: 5432,
-  username:process.env.USER,
-  password:process.env.PASSWORD,
-  database:process.env.DATABASE,
-  logging: true,
+
+  // host: process.env.HOST,
+  // port: 5432,
+  // username:process.env.USER,
+  // password:process.env.PASSWORD,
+  // database:process.env.DATABASE,
+  // logging: true,
   
   synchronize: false,
   entities: [User, Flash],
-  // migrations: ['./src/migration/**/*{.ts,.js}', './build/dist/migration/**/*.js'],
+  migrations: [join(__dirname, '**/migration', '*.{ts,js}')],
+  // migrations: ['./src/migration/**/*.ts'],
   extra: {
     ssl: {
       rejectUnauthorized: false,
